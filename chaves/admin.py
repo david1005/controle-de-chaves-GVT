898d9c93d9a4409f11bf4e8a6aad751a8aa96c7e
Lucas B <lucas.br.oliveiraa@gmail.com>
2026-08-11 16:46:24 -0300

    Criação de Modelos/Pastas do Front

--- chaves/admin.py
+++ chaves/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Chave, Pessoa, Operador, Movimentacao
 
 # Register your models here.
+admin.site.register(Chave)
+admin.site.register(Pessoa)
+admin.site.register(Operador)
+admin.site.register(Movimentacao)
--- chaves/models.py
+++ chaves/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
 # Create your models here.
+class Chave(models.Model):
+    id = models.AutoField(primary_key=True)
+    codigo = models.CharField(max_length=100, unique=True)
+    local = models.CharField(max_length=100)
+    ativa = models.BooleanField(default=True)
+    observacoes = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"Chave {self.codigo} - Local: {self.local} - Ativa: {'Sim' if self.ativa else 'Não'}"
+    
+class Pessoa(models.Model):
+    id = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=100)
+    matricula = models.CharField(max_length=100, unique=True)
+    cargo = models.CharField(max_length=100, blank=True, null=True)
+    foto = models.ImageField(upload_to='fotos_pessoas/', blank=True, null=True)
+
+    def __str__(self):
+        return f"Pessoa {self.nome} - Matrícula: {self.matricula} - Cargo: {self.cargo if self.cargo else 'N/A'}"
+
+class Operador(models.Model):
+    id = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"Operador {self.nome}"
+
+class Movimentacao(models.Model):
+    id = models.AutoField(primary_key=True)
+    chave = models.ForeignKey(Chave, on_delete=models.CASCADE)
+    pessoa = models.ForeignKey(Pessoa, on_delete=models.CASCADE)
+    data_hora_retirada = models.DateTimeField(auto_now_add=True)
+    data_hora_devolucao = models.DateTimeField(blank=True, null=True)
+    operador = models.ForeignKey(Operador, on_delete=models.SET_NULL, related_name='movimentacoes', blank=True, null=True)
+
+    def __str__(self):
+        return f"Movimentação - Chave: {self.chave.codigo} - Pessoa: {self.pessoa.nome} - Retirada: {self.data_hora_retirada} - Devolução: {self.data_hora_devolucao if self.data_hora_devolucao else 'Não devolvida'}"
+
